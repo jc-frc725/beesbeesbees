ae08105e67a9121623bd2d2eef8baa07a260c05e
Justin Chow <hrll71990@gmail.com>
2020-11-30 10:38:51 -0800

    created Bee subclass from Grub superclass, with properties unique to Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+  // use invoke .call on Grub, passing in Bee object itself
+  // keyword new invisibly writes this = Object.create('something'),
+  // thus .call(this) refers to newly made instance of 'something'
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+// Set up Bee's prototype to delegate all calls to 'Car' methods to Car's prototype
+Bee.prototype = Object.create(Grub.prototype);
+
+// Set up Bee's constructor, delegates all calls to a constructor from 'Bee' instances to
+// this newly-made constructor property
+Bee.prototype.constructor = Bee;
+
 
